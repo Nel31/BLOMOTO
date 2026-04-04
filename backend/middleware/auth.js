729e46044c5813bcfd04b2273341573128c39af4
aafissou <afissou.assouma@epitech.eu>
2026-04-04 10:02:50 +0000

    feda

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,8 +1,8 @@
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 // Middleware pour vérifier le token JWT
-export const protect = async (req, res, next) => {
+const protect = async (req, res, next) => {
   try {
     let token;
 
@@ -40,7 +40,7 @@ export const protect = async (req, res, next) => {
 };
 
 // Middleware pour vérifier les rôles
-export const authorize = (...roles) => {
+const authorize = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({
@@ -50,3 +50,5 @@ export const authorize = (...roles) => {
     next();
   };
 };
+
+module.exports = { protect, authorize };
